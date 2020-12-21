ff5b5b7e1fc3d04fccca7abfc935b7de171d15c0
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-21 14:40:50 +0100

    implement JWT Authentication

--- to-do-list-backend/routes/api.php
+++ to-do-list-backend/routes/api.php
@@ -18,4 +18,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('todo-items', 'ToDoItemController');
+Route::resource('todo-items', 'ToDoItemController')->middleware('auth:api');
+
+Route::group([
+
+  'middleware' => 'api',
+  'prefix' => 'auth'
+
+], function ($router) {
+
+  Route::post('login', 'AuthController@login');
+  Route::post('logout', 'AuthController@logout');
+  Route::post('refresh', 'AuthController@refresh');
+  Route::post('me', 'AuthController@me');
+  
+});
